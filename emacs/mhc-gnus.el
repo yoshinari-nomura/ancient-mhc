a77122207d86ff60d34d9d2ff4226da4048c8191
tsuchiya <tsuchiya>
2004-05-01 12:34:37 +0000

    Rewritten to support both original Gnus and T-gnus.

--- emacs/mhc-gnus.el
+++ emacs/mhc-gnus.el
@@ -12,6 +12,15 @@
 ;; This file is a part of MHC, and includes MUA backend methods for
 ;; Gnus.
 
+;; When you use T-gnus, you should add the following expression to
+;; your ~/.emacs.
+;;
+;;     (add-hook 'mhc-draft-mode-hook 'mime-edit-mode)
+;;
+;; Otherwise, you should add the following expression to your
+;; ~/.emacs.
+;;
+;;     (add-hook 'mhc-draft-mode-hook 'mml-mode)
 
 ;;; Code:
 
@@ -21,65 +30,65 @@
 (require 'gnus-sum)
 (require 'nnmhc)
 
+;; To suppress byte-compile warnings.
+(eval-and-compile
+  (defvar gnus-original-article-buffer)
+  (autoload 'gnus-copy-article-buffer "gnus-msg")
+  (autoload 'mime-to-mml "mml")
+  (autoload 'rfc2047-decode-region "rfc2047")
+  (autoload 'rfc2047-decode-string "rfc2047")
+  (autoload 'rfc2047-encode-message-header "rfc2047"))
 
-;; Internal Variables:
+
+;;; Internal Variables:
 
 (defvar mhc-gnus/mhc-is-running nil)
 
 
-(if (string-match "SEMI" gnus-version)
-    (require 'mhc-mime))
-
-
-;; Setup function:
+;;; Setup function:
 
 ;;;###autoload
 (defun mhc-gnus-setup ()
   (require 'mhc)
   (setq mhc-mailer-package 'gnus)
+  (mhc-gnus/setup-methods)
   (mhc-setup)
   (add-hook 'gnus-group-mode-hook 'mhc-mode)
   (add-hook 'gnus-summary-mode-hook 'mhc-mode)
   (add-hook 'gnus-exit-gnus-hook 'mhc-exit))
 
 
-;; Backend methods:
+;;; Backend methods:
 
+;;; Summary APIs (mhc-summary.el):
 (defun mhc-gnus-summary-filename ()
   "Return the file name of the article on the current line."
   (let ((num (get-text-property (point) 'gnus-number)))
     (if num (nnmhc-get-article num))))
 
-
 (defun mhc-gnus-summary-display-article ()
-  "Display the article on the current."
+  "Display the article on the current line."
   (let ((num (get-text-property (point) 'gnus-number)))
     (if num (gnus-summary-display-article num))))
 
-
-;; To suppress byte compile warning.
-(autoload 'gnus-copy-article-buffer "gnus-msg")
-
 (defun mhc-gnus-get-import-buffer (get-original)
+  "Return a buffer visiting import article.
+If GET-ORIGINAL is non-nil, return a pair of buffer: one keeps a MIME
+message and the other keeps a visible message.
+
+NOTE: This function designed for original Gnus, not for T-gnus.  When
+using T-gnus, `mhc-mime-get-import-buffer' must be used instead of
+this function."
   (gnus-summary-select-article)
-  (gnus-copy-article-buffer))
-
-
-(defun mhc-gnus-mime-get-raw-buffer ()
-  (gnus-summary-select-article)
-  gnus-original-article-buffer)
-
-
-(defun mhc-gnus-mime-get-mime-structure ()
-  (gnus-summary-select-article)
-  gnus-current-headers)
-
+  (if get-original
+      (cons gnus-original-article-buffer gnus-article-buffer)
+    gnus-article-buffer))
 
 (defsubst mhc-gnus/date-to-group-name (date)
   (mhc-date-format date "%s/%02d/%02d" mhc-base-folder yy mm))
 
-
 (defun mhc-gnus-generate-summary-buffer (date)
+  "Generate a summary buffer for DATE, and change current buffer to it."
   (let* ((group (mhc-gnus/date-to-group-name date))
 	 (method `(nnmhc ,group))
 	 (vgroup (gnus-group-prefixed-name group method)))
@@ -95,16 +104,15 @@
 	  nnmhc-article-list nil)
     (delete-region (point-min) (point-max))))
 
-
-(autoload 'eword-encode-string "eword-encode")
-(autoload 'rfc2047-encode-string "rfc2047")
-(eval-and-compile
-  (defalias 'mhc-gnus-encode-string
-    (if (featurep 'mhc-mime) 'eword-encode-string 'rfc2047-encode-string)))
+;; This is a trick to suppress byte-compile of the inline function
+;; `mail-full-mail-header' defined by `defsubst'.  Cf. (ELF:01937)
+(defalias 'mhc-gnus/make-full-mail-header 'make-full-mail-header)
+(defalias 'mhc-gnus/encode-string 'identity)
 
 (defun mhc-gnus-insert-summary-contents (inserter)
+  "Insert `mhc-tmp-schedule' with INSERTER."
   (let ((x (mhc-record-name (mhc-schedule-record mhc-tmp-schedule)))
-	(subject (mhc-gnus-encode-string
+	(subject (mhc-gnus/encode-string
 		  (mhc-schedule-subject-as-string mhc-tmp-schedule)))
 	(pos (point)))
     (when x
@@ -112,14 +120,14 @@
       (setq x (length nnmhc-article-list)))
     (funcall inserter)
     (if x
-	(let ((header (funcall (symbol-function 'make-full-mail-header) x subject)))
+	(let ((header (mhc-gnus/make-full-mail-header x subject)))
 	  (put-text-property pos (point) 'gnus-number x)
 	  (push (gnus-data-make x 0 0 header 0) gnus-newsgroup-data))
       (remove-text-properties pos (point) '(gnus-number nil)))
     (insert "\n")))
 
-
 (defun mhc-gnus-summary-mode-setup (date)
+  "Setup this current buffer as a summary buffer for DATE."
   (setq gnus-newsgroup-data (nreverse gnus-newsgroup-data)
 	nnmhc-article-list (nreverse nnmhc-article-list))
   (save-excursion
@@ -140,22 +148,107 @@
   (setq gnus-newsgroup-begin 1
 	gnus-newsgroup-end (length nnmhc-article-list)))
 
-
 (defun mhc-gnus-highlight-message (for-draft)
+  "Hilight message in the current buffer.
+If FOR-DRAFT is non-nil, Hilight message as draft message."
   (let ((gnus-article-buffer (current-buffer))
 	;; Adhoc fix to avoid errors in gnus-article-add-buttons().
 	(gnus-button-marker-list))
     (gnus-article-highlight)))
 
-(autoload 'rfc2047-decode-string "rfc2047")
 (defalias 'mhc-gnus-decode-string 'rfc2047-decode-string)
 
-(autoload 'rfc2047-decode-region "rfc2047")
 (defun mhc-gnus-decode-header ()
-  "Alternative function of `decode-header-in-buffer' for pure Gnus."
+  "Decode MIME-encoded headers.
+
+NOTE: This function designed for original Gnus, not for T-gnus.  When
+using T-gnus, `mhc-mime-decode-header' must be used instead of this
+function."
   (goto-char (point-min))
   (when (re-search-forward "^$" nil t)
     (rfc2047-decode-region (point-min) (point))))
+
+
+;;; Draft APIs (mhc-draft.el):
+(defun mhc-gnus-draft-setup-new ()
+  "Setup new draft (Insert header separator).
+
+NOTE: This function designed for original Gnus, not for T-gnus.  When
+using T-gnus, `mhc-mime-draft-setup-new' must be used instead of this
+function."
+  (goto-char (point-min))
+  (insert mail-header-separator "\n"))
+
+(defun mhc-gnus-draft-reedit-buffer (buffer original)
+  "Prepare a draft from the content of the BUFFER.
+If ORIGINAL is non-nil, this function converts a MIME message in the
+BUFFER into a human-editable text written in MML, a language for
+describing MIME parts.
+
+NOTE: This function designed for original Gnus, not for T-gnus.  When
+using T-gnus, `mhc-mime-draft-reedit-buffer' must be called instead of
+this function."
+  ;; If current buffer is specified as buffer, no need to replace.
+  (unless (eq (current-buffer) buffer)
+    (erase-buffer)
+    (insert-buffer buffer))
+  (when original
+    ;; buffer is raw buffer.
+    (mime-to-mml))
+  (mhc-header-narrowing
+    (mhc-header-delete-header
+     "^\\(Content-.*\\|Mime-Version\\|User-Agent\\):" 'regexp))
+  (goto-char (point-min))
+  (when (re-search-forward "^$" nil t)
+    (insert mail-header-separator)))
+
+(defun mhc-gnus-draft-reedit-file (file)
+  "Prepare a draft from the FILE.
+
+NOTE: This function designed for original Gnus, not for T-gnus.  When
+using T-gnus, `mhc-mime-draft-reedit-file' must be called instead of
+this function."
+  (erase-buffer)
+  (let ((coding-system-for-read 'raw-text-dos)
+	(format-alist))
+    (insert-file-contents file))
+  (mhc-gnus-draft-reedit-buffer (current-buffer) t))
+
+(defun mhc-gnus-draft-translate ()
+  "Convert a message in this current buffer to a MIME message.
+A input text in this current buffer must be written in MML, a language
+for describing MIME parts.
+
+NOTE: This function designed for original Gnus, not for T-gnus.  When
+using T-gnus, `mhc-mime-draft-translate' must be called instead of this
+function."
+  (message-encode-message-body)
+  (save-restriction
+    (message-narrow-to-headers)
+    (let ((message-newsreader (gnus-extended-version)))
+      (message-generate-headers '(User-Agent Lines)))
+    (message-remove-header message-ignored-mail-headers t)
+    (let ((mail-parse-charset message-default-charset))
+      (rfc2047-encode-message-header)))
+  (save-excursion
+    (goto-char (point-min))
+    (when (search-forward (concat "\n" mail-header-separator "\n") nil t)
+      (replace-match "\n\n"))))
+
+
+;;; MIME APIs (mhc-mime.el):
+(defun mhc-gnus-mime-get-raw-buffer ()
+  "Get raw buffer of the current message.
+Note: This function is only used when using T-gnus."
+  (gnus-summary-select-article)
+  gnus-original-article-buffer)
+
+(defun mhc-gnus-mime-get-mime-structure ()
+  "Get mime message structure of the current message.
+Note: This function is only used when using T-gnus."
+  (gnus-summary-select-article)
+  gnus-current-headers)
+
 
 ;; modify Gnus original functions for cursor control.
 (eval-after-load "gnus"
@@ -163,55 +256,66 @@
      (around mhc-gnus-summary-position-point activate compile)
      (or mhc-gnus/mhc-is-running ad-do-it)))
 
-(eval-after-load "gnus-sum"
-  '(defadvice gnus-summary-update-mark
-     (around mhc-gnus-summary-update-mark activate compile)
-     (or mhc-gnus/mhc-is-running ad-do-it)))
+(let (current-load-list)
+  (defadvice gnus-summary-update-mark
+    (around mhc-gnus-summary-update-mark activate compile)
+    (or mhc-gnus/mhc-is-running ad-do-it)))
 
 ;; modify Gnus original commands for manipulate articles.
-(eval-after-load "gnus-sum"
-  '(defadvice gnus-summary-edit-article
-     (around mhc-gnus-draft-edit-message activate compile)
-     "If MHC is running, exec mhc-modify."
-     (if mhc-gnus/mhc-is-running
-	 (mhc-modify)
-       ad-do-it)))
+(let (current-load-list)
+  (defadvice gnus-summary-edit-article
+    (around mhc-gnus-draft-edit-message activate compile)
+    "If MHC is running, call `mhc-modify'."
+    (if mhc-gnus/mhc-is-running
+	(mhc-modify)
+      ad-do-it)))
 
-(eval-after-load "gnus-sum"
-  '(defadvice gnus-summary-delete-article
-     (around mhc-gnus-summary-delete-article activate compile)
-     "If MHC is running, exec mhc-delete."
-     (if mhc-gnus/mhc-is-running
-	 (mhc-delete)
-       ad-do-it)))
-
+(let (current-load-list)
+  (defadvice gnus-summary-delete-article
+    (around mhc-gnus-summary-delete-article activate compile)
+    "If MHC is running, call `mhc-delete'."
+    (if mhc-gnus/mhc-is-running
+	(mhc-delete)
+      ad-do-it)))
 
 
 (provide 'mhc-gnus)
 (put 'mhc-gnus 'summary-filename 'mhc-gnus-summary-filename)
 (put 'mhc-gnus 'summary-display-article 'mhc-gnus-summary-display-article)
-(put 'mhc-gnus 'get-import-buffer 'mhc-gnus-get-import-buffer)
 (put 'mhc-gnus 'generate-summary-buffer 'mhc-gnus-generate-summary-buffer)
 (put 'mhc-gnus 'insert-summary-contents 'mhc-gnus-insert-summary-contents)
-(put 'mhc-gnus 'summary-search-date 'mhc-gnus-summary-search-date)
 (put 'mhc-gnus 'summary-mode-setup 'mhc-gnus-summary-mode-setup)
 (put 'mhc-gnus 'highlight-message 'mhc-gnus-highlight-message)
 
-(when (featurep 'mhc-mime)
-  (put 'mhc-gnus 'draft-setup-new 'mhc-mime-draft-setup-new)
-  (put 'mhc-gnus 'draft-reedit-buffer 'mhc-mime-draft-reedit-buffer)
-  (put 'mhc-gnus 'draft-reedit-file 'mhc-mime-draft-reedit-file)
-  (put 'mhc-gnus 'get-import-buffer 'mhc-mime-get-import-buffer)
-  (put 'mhc-gnus 'mime-get-raw-buffer 'mhc-gnus-mime-get-raw-buffer)
-  (put 'mhc-gnus 'mime-get-mime-structure 'mhc-gnus-mime-get-mime-structure)
-  (put 'mhc-gnus 'draft-translate 'mhc-mime-draft-translate))
-
-(if (featurep 'mhc-mime)
-    (progn
-      (put 'mhc-gnus 'decode-header 'mhc-mime-decode-header)
-      (put 'mhc-gnus 'eword-decode-string 'mhc-mime-eword-decode-string))
-  (put 'mhc-gnus 'decode-header 'mhc-gnus-decode-header)
-  (put 'mhc-gnus 'eword-decode-string 'mhc-gnus-decode-string))
+(defun mhc-gnus/setup-methods ()
+  (if (and (boundp 'gnus-version)
+	   (stringp (symbol-value 'gnus-version))
+	   (string-match "SEMI" (symbol-value 'gnus-version)))
+      (progn
+	(require 'mhc-mime)
+	(autoload 'eword-encode-string "eword-encode")
+	(defalias 'mhc-gnus/encode-string 'eword-encode-string)
+	(put 'mhc-gnus 'draft-setup-new 'mhc-mime-draft-setup-new)
+	(put 'mhc-gnus 'draft-reedit-buffer 'mhc-mime-draft-reedit-buffer)
+	(put 'mhc-gnus 'draft-reedit-file 'mhc-mime-draft-reedit-file)
+	(put 'mhc-gnus 'draft-translate 'mhc-mime-draft-translate)
+	(put 'mhc-gnus 'get-import-buffer 'mhc-mime-get-import-buffer)
+	(put 'mhc-gnus 'decode-header 'mhc-mime-decode-header)
+	(put 'mhc-gnus 'eword-decode-string 'mhc-mime-eword-decode-string)
+	(put 'mhc-gnus 'mime-get-raw-buffer 'mhc-gnus-mime-get-raw-buffer)
+	(put 'mhc-gnus 'mime-get-mime-structure 'mhc-gnus-mime-get-mime-structure))
+    (autoload 'rfc2047-encode-string "rfc2047")
+    (defalias 'mhc-gnus/encode-string 'rfc2047-encode-string)
+    (put 'mhc-gnus 'draft-setup-new 'mhc-gnus-draft-setup-new)
+    (put 'mhc-gnus 'draft-reedit-buffer 'mhc-gnus-draft-reedit-buffer)
+    (put 'mhc-gnus 'draft-reedit-file 'mhc-gnus-draft-reedit-file)
+    (put 'mhc-gnus 'draft-translate 'mhc-gnus-draft-translate)
+    (put 'mhc-gnus 'get-import-buffer 'mhc-gnus-get-import-buffer)
+    (put 'mhc-gnus 'decode-header 'mhc-gnus-decode-header)
+    (put 'mhc-gnus 'eword-decode-string 'mhc-gnus-decode-string)
+    (put 'mhc-gnus 'mime-get-raw-buffer nil)
+    (put 'mhc-gnus 'mime-get-mime-structure nil)))
+(mhc-gnus/setup-methods)
 
 ;;; Copyright Notice:
 
@@ -221,7 +325,7 @@
 ;; Redistribution and use in source and binary forms, with or without
 ;; modification, are permitted provided that the following conditions
 ;; are met:
-;; 
+;;
 ;; 1. Redistributions of source code must retain the above copyright
 ;;    notice, this list of conditions and the following disclaimer.
 ;; 2. Redistributions in binary form must reproduce the above copyright
@@ -230,7 +334,7 @@
 ;; 3. Neither the name of the team nor the names of its contributors
 ;;    may be used to endorse or promote products derived from this software
 ;;    without specific prior written permission.
-;; 
+;;
 ;; THIS SOFTWARE IS PROVIDED BY THE TEAM AND CONTRIBUTORS ``AS IS''
 ;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 ;; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
