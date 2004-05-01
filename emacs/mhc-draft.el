6d87c40abcc6989b87d4d7c51751e0a4d8d0697e
tsuchiya <tsuchiya>
2004-05-01 12:55:23 +0000

    (mhc-draft-unuse-hdr-list): Add some headers.

--- emacs/mhc-draft.el
+++ emacs/mhc-draft.el
@@ -3,7 +3,7 @@
 ;; Author:  Yoshinari Nomura <nom@quickhack.net>,
 ;;          Yuuichi Teranishi <teranisi@quickhack.net>
 ;; Created: 2000/07/25
-;; Revised: $Date: 2000/08/10 03:58:02 $
+;; Revised: $Date: 2004/05/01 12:55:23 $
 
 ;;; Commentary:
 
@@ -43,11 +43,11 @@
 (defconst mhc-draft-buffer-name "*mhc draft*")
 
 (defcustom mhc-draft-unuse-hdr-list
-  '(">From " "From " "Delivered-To:" "Errors-To:" "Lines:"
-    "Posted:" "Precedence:" "Received:" "Replied:" "Return-Path:"
-    "Sender:" "User-Agent:" "X-Dispatcher:" "X-Filter:"
-    "X-Gnus-Mail-Source:" "X-Mailer:" "X-Received:" "X-Sender:"
-    "X-Seqno:" "Xref:")
+  '(">From " "From " "Delivered-To:" "Delivery-date:" "Envelope-to:"
+    "Errors-To:" "Lines:" "Posted:" "Precedence:" "Received:" "Replied:"
+    "Return-Path:" "Sender:" "User-Agent:" "X-Bogosity:" "X-Dispatcher:"
+    "X-Filter:" "X-Gnus-Mail-Source:" "X-Mailer:" "X-Received:" "X-Sender:"
+    "X-Seqno:" "X-Spam-Flag:" "X-Spam-Probability:" "X-UIDL:" "Xref:")
   "*These headers are removed when article is imported."
   :group 'mhc
   :type '(repeat string))
